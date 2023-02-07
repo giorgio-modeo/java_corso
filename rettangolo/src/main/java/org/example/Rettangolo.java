a8bb81ecd7e299bcbcf3aecca73335ee5ae957fe
giorgio-modeo <75391952+giorgio-modeo@users.noreply.github.com>
2023-02-07 16:00:22 +0100

    create rettangolo

---
+++ rettangolo/src/main/java/org/example/Rettangolo.java
@@ -0,0 +1,10 @@
+public class Rettangolo {
+    int base;
+    int altezza;
+    final int NUMERO_LATI = 4;
+
+    public int calcolaArea(int base, int altezza){
+        int area = base * altezza;
+        return area;
+    }
+}
